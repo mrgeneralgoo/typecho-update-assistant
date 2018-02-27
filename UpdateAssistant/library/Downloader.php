a0bea1c355424db2299ee34ff1b32176daaa310a
generalchen <mrgeneralgoo@gmail.com>
2018-02-27 23:23:46 +0800

    Optimize logic

--- UpdateAssistant/library/Downloader.php
+++ UpdateAssistant/library/Downloader.php
@@ -15,6 +15,11 @@ class Downloader extends Base
         $downloadUrl     = 'https://github.com/typecho/typecho/archive/master.zip';
         $archiveRealPath = rtrim($workRootPath, '/.\\') . DIRECTORY_SEPARATOR . 'archive' . DIRECTORY_SEPARATOR . $archiveName . '.zip';
 
+        // cache
+        if (is_file($archiveRealPath)) {
+            return true;
+        }
+
         if (!$isDevelop) {
             $content = $this->{$this->handler}('http://typecho.org/version.json');
 
@@ -27,11 +32,6 @@ class Downloader extends Base
             }
 
             $downloadUrl = sprintf('https://github.com/typecho/typecho/archive/v%s-%s-release.zip', $content['release'], $content['version']);
-        }
-
-        // cache
-        if (is_file($archiveRealPath)) {
-            return true;
         }
 
         return $this->{$this->handler}($downloadUrl, $archiveRealPath);
